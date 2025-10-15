8bb3145577a71a2862f4f43b4324090ac5a45bcc
015Bharathi <bharathikumar6369@gmail.com>
2025-10-15 19:48:00 +0530

    React Fundamentals: JSX

---
+++ React/Fundamentals/src/components/01-JSX/JSXExamples.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const JSXExamples: React.FC = () => {
+    return <div>Let's Learn JSX</div>;
+//   return React.createElement("div", null, "Let's Learn JSX");
+//   return React.createElement(
+//     "div",
+//     { style: { color: "black" } },
+//     "Let's Learn JSX"
+//   );
+};
+
+export default JSXExamples;
